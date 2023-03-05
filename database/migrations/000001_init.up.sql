e617d0741adb0906aeff038f732eccb3697f6472
pakoszg <gergelypakosz@gmail.com>
2023-03-05 21:42:49 +0100

    chore: create postgrest roles

--- database/migrations/000001_init.up.sql
+++ database/migrations/000001_init.up.sql
@@ -2,6 +2,17 @@ BEGIN;
 
 SET search_path = public, new_gold;
 
+CREATE ROLE postgrest_authenticator noinherit LOGIN PASSWORD 'qwerty12345';
+GRANT usage ON SCHEMA new_gold TO postgrest_authenticator;
+
+CREATE ROLE postgrest_anon nologin;
+GRANT usage ON SCHEMA new_gold TO postgrest_anon;
+GRANT postgrest_anon TO postgrest_authenticator;
+
+CREATE ROLE postgrest_user nologin;
+GRANT usage ON SCHEMA new_gold TO postgrest_user;
+GRANT postgrest_user TO postgrest_authenticator;
+
 CREATE TABLE new_gold.users (
     id uuid PRIMARY KEY,
 	created_at TIMESTAMP NOT NULL,
@@ -10,5 +21,7 @@ CREATE TABLE new_gold.users (
 	email text UNIQUE NOT NULL,
     last_login TIMESTAMP
 );
+GRANT SELECT ON new_gold.users TO postgrest_user;
+GRANT SELECT, UPDATE, INSERT, DELETE ON new_gold.users to postgrest_authenticator;
 
 COMMIT;
