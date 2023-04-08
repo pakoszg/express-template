064f8275019cb1a96e70fcd12b54bef683129e9b
pakoszg <gergelypakosz@gmail.com>
2023-04-08 20:47:33 +0200

    chore: move async handler

--- express/src/index.ts
+++ express/src/index.ts
@@ -1,9 +1,19 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, {
+  NextFunction,
+  Request,
+  RequestHandler,
+  Response,
+} from "express";
 import user from "./routes/user";
 import errorMiddleware, { HttpStatusCode, IHTTPError } from "./error";
 
 const app = express();
 const port = 8000;
+
+export const asyncHandler =
+  (fn: RequestHandler) =>
+  async (req: Request, res: Response, next: NextFunction) =>
+    Promise.resolve(fn(req, res, next)).catch(next);
 
 app.use(express.json()); // for application/json
 app.use(express.urlencoded()); // for application/x-www-form-urlencoded
--- express/src/routes/user.ts
+++ express/src/routes/user.ts
@@ -5,15 +5,10 @@ import express, {
   RequestHandler,
 } from "express";
 import createUser from "../https/user/createUser.f";
+import { asyncHandler } from "..";
 
 const router = express.Router();
 
-const asyncHandler =
-  (fn: RequestHandler) =>
-  async (req: Request, res: Response, next: NextFunction) =>
-    Promise.resolve(fn(req, res, next)).catch(next);
-
-// define the home page route
 router.post("/create", asyncHandler(createUser));
 
 export default router;
